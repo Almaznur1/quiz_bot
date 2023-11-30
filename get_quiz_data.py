be8c6435183d4f806c27e1101c79a2853ad4e931
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-11-30 23:47:23 +0300

    Update get_quiz_data.py

--- get_quiz_data.py
+++ get_quiz_data.py
@@ -2,11 +2,12 @@ def get_quiz_data(path):
     with open(path, 'r', encoding='koi8-r') as file:
         quiz_data = {}
         text_blocks = file.read().split('\n\n')
-        for block in text_blocks:
-            if block.startswith('Вопрос '):
-                question = ' '.join(block.split('\n')[1:])
-            elif block.startswith('Ответ:'):
-                answer = block.split('\n')[1]
-                quiz_data[question] = answer
+
+    for block in text_blocks:
+        if block.startswith('Вопрос '):
+            question = ' '.join(block.split('\n')[1:])
+        elif block.startswith('Ответ:'):
+            answer = block.split('\n')[1]
+            quiz_data[question] = answer
 
     return quiz_data
